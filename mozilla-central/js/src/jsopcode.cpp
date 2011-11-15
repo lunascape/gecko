6f5d1504a4ada096ac2d49a84ee2dc9ad1fc772f
unknown <nobuo.nakata@.ad.lunascape.co.jp>
2011-11-15 17:37:20 +0900

    lunagecko 3.6.23

--- mozilla-central/js/src/jsopcode.cpp
+++ mozilla-central/js/src/jsopcode.cpp
@@ -156,13 +156,11 @@ js_GetIndexFromBytecode(JSContext *cx, JSScript *script, jsbytecode *pc,
     span = js_CodeSpec[op].length;
     base = 0;
     if (pc - script->code + span < script->length) {
-        JSOp next = js_GetOpcode(cx, script, pc + span);
-        if (next == JSOP_RESETBASE) {
+        if (pc[span] == JSOP_RESETBASE) {
             base = GET_INDEXBASE(pc - JSOP_INDEXBASE_LENGTH);
-        } else if (next == JSOP_RESETBASE0) {
-            JSOp prev = js_GetOpcode(cx, script, pc - 1);
-            JS_ASSERT(JSOP_INDEXBASE1 <= prev && prev <= JSOP_INDEXBASE3);
-            base = (prev - JSOP_INDEXBASE1 + 1) << 16;
+        } else if (pc[span] == JSOP_RESETBASE0) {
+            JS_ASSERT(JSOP_INDEXBASE1 <= pc[-1] || pc[-1] <= JSOP_INDEXBASE3);
+            base = (pc[-1] - JSOP_INDEXBASE1 + 1) << 16;
         }
     }
     return base + GET_UINT16(pc + pcoff);
